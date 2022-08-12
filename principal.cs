45c24fec15a3cba34e075d1428ac69d0d40a07e2
Monica Campos <USSS005122@ugb.edu.sv>
2022-08-12 12:58:36 -0500

    Agregar archivos de proyecto.

---
+++ principal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_3_proyecto_consola_menu
+{
+    class principal
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
